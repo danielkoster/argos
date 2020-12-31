258feed4738497b0d09c9e30471bbca86f32cc4f
Daniel Koster <d.koster@iwink.nl>
2020-12-31 13:56:40 +0100

    Add torrent to client

---
+++ src/EventSubscriber/DownloadNotificationSubscriber.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\Episode;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Events;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
+
+/**
+ * Sends notifications about downloaded episodes.
+ */
+class DownloadNotificationSubscriber implements EventSubscriber {
+	/**
+	 * Symfony's notifier.
+	 * @var NotifierInterface
+	 */
+	private NotifierInterface $notifier;
+
+	/**
+	 * Create an event subscriber.
+	 * @param NotifierInterface $notifier
+	 */
+	public function __construct(NotifierInterface $notifier) {
+		$this->notifier = $notifier;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getSubscribedEvents(): array {
+		return [
+			Events::postPersist,
+		];
+	}
+
+	/**
+	 * Triggered when an entity is persisted the first time.
+	 * @param LifecycleEventArgs $args The arguments.
+	 */
+	public function postPersist(LifecycleEventArgs $args): void {
+		$episode = $args->getEntity();
+		if (!$episode instanceof Episode) {
+			return;
+		}
+
+		$this->notifier->send(new Notification(sprintf(
+			"An episode has been downloaded\!\n%s",
+			str_replace('-', '\\-', (string) $episode)
+		)));
+	}
+}
--- src/Service/DownloadService.php
+++ src/Service/DownloadService.php
@@ -4,9 +4,10 @@ namespace App\Service;
 
 use App\Entity\Episode;
 use App\Entity\EpisodeCandidate;
+use App\Entity\EpisodeInterface;
 use App\Repository\EpisodeRepository;
-use Symfony\Component\Notifier\ChatterInterface;
-use Symfony\Component\Notifier\Message\ChatMessage;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Transmission\Transmission;
 
 /**
@@ -14,37 +15,55 @@ use Transmission\Transmission;
  */
 class DownloadService {
 	/**
-	 * The episode repository.
-	 * @var EpisodeRepository
-	 */
-	private EpisodeRepository $episodeRepository;
-
-	/**
 	 * Transmission client.
 	 * @var Transmission
 	 */
 	private Transmission $transmission;
 
 	/**
-	 * Symfony's chatter.
-	 * @var ChatterInterface
+	 * The episode repository.
+	 * @var EpisodeRepository
 	 */
-	private ChatterInterface $chatter;
+	private EpisodeRepository $episodeRepository;
+
+	/**
+	 * Symfony's filesystem.
+	 * @var Filesystem
+	 */
+	private Filesystem $filesystem;
+
+	/**
+	 * Symfony's HTTP client.
+	 * @var HttpClientInterface
+	 */
+	private HttpClientInterface $httpClient;
+
+	/**
+	 * Base path to store downloaded files.
+	 * @var string
+	 */
+	private string $downloadPath;
 
 	/**
 	 * Create a service.
-	 * @param EpisodeRepository $episodeRepository
 	 * @param Transmission $transmission
-	 * @param ChatterInterface $chatter
+	 * @param EpisodeRepository $episodeRepository
+	 * @param Filesystem $filesystem
+	 * @param HttpClientInterface $httpClient
+	 * @param string $downloadPath
 	 */
 	public function __construct(
-		EpisodeRepository $episodeRepository,
 		Transmission $transmission,
-		ChatterInterface $chatter
+		EpisodeRepository $episodeRepository,
+		Filesystem $filesystem,
+		HttpClientInterface $httpClient,
+		string $downloadPath
 	) {
-		$this->episodeRepository = $episodeRepository;
 		$this->transmission = $transmission;
-		$this->chatter = $chatter;
+		$this->episodeRepository = $episodeRepository;
+		$this->filesystem = $filesystem;
+		$this->httpClient = $httpClient;
+		$this->downloadPath = $downloadPath;
 	}
 
 	/**
@@ -53,7 +72,10 @@ class DownloadService {
 	 */
 	public function downloadEpisodeCandidate(EpisodeCandidate $episodeCandidate): void {
 		// Add torrent to client.
-		//$this->transmission->add();
+		$this->downloadTorrentToPath(
+			$episodeCandidate->getDownloadLink(),
+			$this->generatePath($episodeCandidate)
+		);
 
 		// Store the episode.
 		$episode = (new Episode())
@@ -64,10 +86,36 @@ class DownloadService {
 			->setQuality($episodeCandidate->getQuality());
 
 		$this->episodeRepository->save($episode);
+	}
 
-		$this->chatter->send(new ChatMessage(sprintf(
-			"An episode has been downloaded\!\n%s",
-			str_replace('-', '\\-', (string) $episode)
-		)));
+	/**
+	 * Downloads a torrent to a path.
+	 * @param string $path
+	 * @param string $torrent
+	 */
+	private function downloadTorrentToPath(string $torrent, string $path): void {
+		// Download external torrent to a temporary location.
+		if (strpos($torrent, 'http') === 0) {
+			$torrent = $this->filesystem->tempnam(sys_get_temp_dir(), 'argos_', '.torrent');
+			$this->filesystem->dumpFile(
+				$torrent,
+				$this->httpClient->request('GET', $torrent)->getContent()
+			);
+		}
+
+		$this->transmission->add($torrent, $path);
+	}
+
+	/**
+	 * Generates path to download episode to.
+	 * @param EpisodeInterface $episode
+	 * @return string
+	 */
+	private function generatePath(EpisodeInterface $episode): string {
+		return sprintf(
+			'%s/tv-shows/Season %s/',
+			$this->downloadPath,
+			str_pad($episode->getSeasonNumber(), 2, 0, STR_PAD_LEFT)
+		);
 	}
 }
